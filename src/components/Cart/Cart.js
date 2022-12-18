521e6a34f80c5ff916723068b2fe4989c202fa8b
Jasim Uddin <jasim.ice.iu@gmail.com>
2022-12-18 19:18:22 +0100

    all section of cart added and fixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({ cart }) => {
+const Cart = (props) => {
+    const { cart } = props;
+    console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = (total + shipping + parseFloat(tax)).toFixed(2);
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
+            <p>Total Price:${total}</p>
+            <p>Total Shipping Charges: ${shipping}</p>
+            <p>Tax:${tax}</p>
+            <h4>Grand Total:${grandTotal}</h4>
+
         </div>
     );
 };
