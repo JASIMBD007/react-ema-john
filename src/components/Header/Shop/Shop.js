c191da8967567dcb520fcc89704207dbd2cd6906
Jasim Uddin <jasim.ice.iu@gmail.com>
2022-12-19 22:08:37 +0100

    local storage and cart show added

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../../utilities/fakedb'
 import Cart from '../../Cart/Cart';
 import Product from '../../Product/Product';
 import './Shop.css'
@@ -6,14 +7,45 @@ import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
+
     useEffect(() => {
+        // console.log('Product load before fetch', products);
         fetch('products.json')
             .then(response => response.json())
             .then(data => setProducts(data))
     }, []);
-    const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+
+    useEffect(() => {
+        // console.log('Local storage first line ');
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for (const id in storedCart) {
+            // console.log(storedCart);
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products]);
+
+    const handleAddToCart = (selectedProduct) => {
+        let newCart = [];
+        const exist = cart.find(product => product.id === selectedProduct.id);
+        if (!exist) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exist.quantity = exist.quantity + 1;
+            newCart = [...rest, exist];
+        }
         setCart(newCart);
+        addToDb(selectedProduct.id);
+
     };
     return (
         <div className='shop-container'>
